d3e0b137725453e740107f9c101f698d10fbfade
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-10-20 17:52:59 +0100

    Main class change

--- src/main/java/com/nigerians/scamazone/ScamazoneApplication.java
+++ src/main/java/com/nigerians/scamazone/ScamazoneApplication.java
@@ -1,12 +1,10 @@
 package com.nigerians.scamazone;
 
-import com.nigerians.scamazone.controllers.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScamazoneApplication {
-    private static final UserController userController = new UserController();
     public static void main(String[] args) {
         SpringApplication.run(ScamazoneApplication.class, args);
     }
